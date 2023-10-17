12bef8cd50c033e103c54152db084f8036b3c2f4
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2023-10-17 09:36:58 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Demo, Demo1 } from "./Demo";
+function App() {
+  return (
+    <>
+      <h1>Hello Vite + React!</h1>
+      <hr></hr>
+      <Demo />
+      <Demo1 />
+    </>
+  );
+}
+
+export default App;
+
+
---
+++ src/Demo.jsx
@@ -0,0 +1,17 @@
+function Demo() {
+  return (
+    <>
+      <h1> Hello Through Demo Components</h1>
+    </>
+  );
+}
+
+function Demo1() {
+    return (
+      <>
+        <h1> Hello Through Demo111111 Components</h1>
+      </>
+    );
+  }
+
+export {Demo,Demo1};
